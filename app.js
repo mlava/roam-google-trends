a4781d8bc628c967af7c6d11070f24cbad8d1c9d
mlava <NxmyLL@8TR30Xf9FQw9ukh6CQlx7&g7K>
2021-02-02 12:53:42 +1100

    Add CORS

--- app.js
+++ app.js
@@ -1,8 +1,8 @@
 const googleTrends = require('./node_modules/google-trends-api');
-const express = require('express')
-var cors = require('cors')
-const app = express()
-const port = 3000
+const express = require('express');
+var cors = require('cors');
+const app = express();
+var port = process.env.PORT || 3000;
 
 var corsOptions = {
   origin: 'https://roamresearch.com',
